ccc28c3244f0c0822ee3a07ccdf9b6dae6963760
ketjj <jorjolianiket@gmail.com>
2022-08-02 12:10:31 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /* Scrivi un programma che stampi in console i numeri da 1 a 100.*/
 
+
 // **MILESTONE 1**
 // Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
@@ -11,32 +12,29 @@
 // Applica uno stile differente a seconda del valore dell’indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
 
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
 const limit = 100;
 
-for(let i=1; i <= limit; i++){
-  const boxx = document.createElement("div");
-  boxx.className = "box";
-  container.append(boxx)
+for(let i = 1; i <= limit; i++){
+
+  const boxEl = document.createElement("div");
+  boxEl.className = "box";
+  //con innerHTML potevi appendere anche da qui boxEl.append(i), senza la condizione else
 
 
-  if(i % 3 === 0 && i % 5 === 0){
-    boxx.innerHTML = 'FizzBuzz';
-    boxx.classList.add("fizzandbuzzcolor");
-
-  } else if(i % 5 === 0) {
-    boxx.append('Buzz');
-    boxx.classList.add("buzzcolor");
-
-  } else if (i % 3 === 0) {
-    boxx.append('Fizz');
-    boxx.classList.add("fizzcolor");
+  if(i % 15 === 0){ //!(i % 15)--> potevi scrivere anche così, perchè 0 = false, not ! true = false( 0 )
+    boxEl.append('fizzbuzz'); //boxEl.innerHTML = 'fizzbizz'; 
+    boxEl.classList.add('fizzandbuzzcolor');
+  }else if(i % 3 === 0){
+    boxEl.append('fizz');
+    boxEl.classList.add('fizzcolor');
+  } else if(i % 5 === 0){
+    boxEl.append('buzz');
+    boxEl.classList.add('buzzcolor');
+  }else {
+    boxEl.append(i)
   }
-  else {
-    boxx.append(i)
-  }
-  
+
+  container.append(boxEl);
 }
-
-
